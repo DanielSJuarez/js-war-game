40350d0616315557b927c262f6e2bf291b271862
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-12 13:13:55 -0500

    inital commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const startGame = document.querySelector('.startGame');
+const addPlayer = document.querySelector('.addPlayer');
+const drawCard = document.querySelector('.drawCard');
+const resetGame = document.querySelector('.resetGame');
+
