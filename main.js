0e4081445346ac3ee7bb14b2f73523da3716df0c
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-12 13:24:39 -0500

    intial html structure

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-const startGame = document.querySelector('.startGame');
 const addPlayer = document.querySelector('.addPlayer');
+const playerName = document.querySelector('.playerName');
+const submitPlayer = document.querySelector('.submitPlayer');
+const startGame = document.querySelector('.startGame');
 const drawCard = document.querySelector('.drawCard');
 const resetGame = document.querySelector('.resetGame');
+
+function Player({name = 'Player',  } = {}){
+    this.name = name;
+}
+
+function Deck({} = {}){
+
+}
+
+function Card({} = {}){
+
+}
+
+function Game({} = {}){
+
+}
 
